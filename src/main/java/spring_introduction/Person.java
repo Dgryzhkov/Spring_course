530e855c6a3ef5229aae38ed90bb6548433e793e
Dgryzhkov <dgryzhkov@gmail.com>
2022-07-28 21:45:47 +0700

    diP4

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -5,11 +5,33 @@ package spring_introduction;
  */
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+
+        System.out.println("Class person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class person: set age");
+        this.age = age;
+    }
 
     /*    public Person(Pet pet) {
-            System.out.println("Person bean is created");
-            this.pet = pet;
-        }*/
+                System.out.println("Person bean is created");
+                this.pet = pet;
+            }*/
     public Person() {
         System.out.println("Person bean is created");
     }
