0bd7bc8ed5f1293942e8fe7c70bb2a3ca76250f8
Dgryzhkov <dgryzhkov@gmail.com>
2022-07-29 10:30:42 +0700

    annotation_config

--- src/main/java/spring_introduction/Cat.java
+++ src/main/java/spring_introduction/Cat.java
@@ -1,8 +1,11 @@
 package spring_introduction;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author Dgryzhkov
  */
+@Component("catBean")
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Cat bean is created");
