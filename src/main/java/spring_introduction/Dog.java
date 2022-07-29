90143b3c81ab62f6eccdb3ab45d647da95f35e49
Dgryzhkov <dgryzhkov@gmail.com>
2022-07-29 10:18:16 +0700

    init_destroy

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -4,22 +4,29 @@ package spring_introduction;
  * @author Dgryzhkov
  */
 public class Dog implements Pet {
-    private String name;
+    //private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public Dog() {
         System.out.println("Dog bean is created");
     }
 
     @Override
-    public void say(){
+    public void say() {
         System.out.println("Bow-Wow");
+    }
+
+    public void init(){
+        System.out.println("Class dog: init method");
+    }
+    public void destroy(){
+        System.out.println("Class dog: destroy method");
     }
 }
