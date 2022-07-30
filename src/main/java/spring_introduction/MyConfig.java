f7d9c93f000f0b7e786316393f3f6e64c61d254d
Dgryzhkov <dgryzhkov@gmail.com>
2022-07-30 12:45:15 +0700

    config_java_code_p1

---
+++ src/main/java/spring_introduction/MyConfig.java
@@ -0,0 +1,12 @@
+package spring_introduction;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Dgryzhkov
+ */
+@Configuration
+@ComponentScan("spring_introduction")
+public class MyConfig {
+}
