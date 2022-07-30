2f53347d502cb463787d38e88a9259227bf1be6b
Dgryzhkov <dgryzhkov@gmail.com>
2022-07-30 11:59:13 +0700

    value

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -2,6 +2,7 @@ package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,7 +14,9 @@ public class Person {
 //    @Autowired
 //    @Qualifier("catBean")
     private Pet pet;
+    @Value("${person.surname}")
     private String surname;
+    @Value("${person.age}")
     private int age;
 
     public String getSurname() {
