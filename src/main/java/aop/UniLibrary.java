fd260d9acbbd3facd2fe3377339a9f34c7a3bdaf
Dgryzhkov <dgryzhkov@gmail.com>
2022-07-30 15:31:10 +0700

    aop_pointcut_p1

---
+++ src/main/java/aop/UniLibrary.java
@@ -0,0 +1,22 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Dgryzhkov
+ */
+@Component()
+public class UniLibrary extends AbstractLibrary {
+    @Override
+    public void getBook(){
+        System.out.println("Мы берем книгу из UniLibrary");
+    }
+
+    public void returnBook(){
+        System.out.println("Возврващаем книгу из UniLibrary");
+    }
+
+    public void getMagazine(){
+        System.out.println("Мы берем журнал из UniLibrary");
+    }
+}
