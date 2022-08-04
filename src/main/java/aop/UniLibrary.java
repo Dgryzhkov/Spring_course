747b294461d642a10fc29f5f843ab6c156562512
Dgryzhkov <dgryzhkov@gmail.com>
2022-08-04 12:34:01 +0700

    around_p2

--- src/main/java/aop/UniLibrary.java
+++ src/main/java/aop/UniLibrary.java
@@ -14,6 +14,7 @@ public class UniLibrary extends AbstractLibrary {
     }
 
     public String returnBook() {
+        int a =10/0;
         System.out.println("Возврващаем книгу в UniLibrary");
         return "Война и мир";
     }
